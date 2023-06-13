e3ffecdb4b7d4aef2a3866d0da9829821df8c3c3
FrancescoThomasFalanga <francescoasdf3124@gmail.com>
2023-06-13 16:35:02 +0200

    feat: add routes on login + guestController

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\Admin\DishController;
+use App\Http\Controllers\Admin\HomeController;
+use App\Http\Controllers\Admin\OrderController;
+use App\Http\Controllers\Admin\RestaurantController;
+use App\Http\Controllers\Admin\TypeController;
+use App\Http\Controllers\GuestHomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,18 +20,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [GuestHomeController::class, 'home'])->name('home');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::middleware(['auth', 'verified'])->prefix('admin')->name('admin.')->group(function() {
+    
+    Route::get('/', [HomeController::class, 'home'])->name('home');
+    
+    Route::resource('restaurants', RestaurantController::class)->parameters(['restaurants' => 'restaurant:slug']);
+
+    Route::resource('dishes', DishController::class)->parameters(['dishes' => 'dish:slug']);
+
+    Route::resource('types', TypeController::class);
+
+    Route::resource('orders', OrderController::class);
+    
+});
+
 
 require __DIR__.'/auth.php';
