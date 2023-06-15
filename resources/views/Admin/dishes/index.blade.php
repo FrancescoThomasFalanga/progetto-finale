56a9cffe5313382cad5a5a5f127e4bbb8d9758cc
FrancescoThomasFalanga <francescoasdf3124@gmail.com>
2023-06-15 12:44:16 +0200

    fix: delete form

--- resources/views/Admin/dishes/index.blade.php
+++ resources/views/Admin/dishes/index.blade.php
@@ -5,9 +5,7 @@
 
         @foreach ($dishes as $dish)
             <div class="card" style="width: 18rem;">
-                @if (
-                    $dish->cover_image !=
-                        'https://static.vecteezy.com/ti/vettori-gratis/p1/5359703-cibo-icone-pixel-perfetto-illustrazione-vettoriale.jpg')
+                @if ($dish->cover_image != 'https://static.vecteezy.com/ti/vettori-gratis/p1/5359703-cibo-icone-pixel-perfetto-illustrazione-vettoriale.jpg')
                     <img src="{{ asset('storage/' . $dish->cover_image) }}" class="card-img-top" alt="...">
                 @else
                     <img src="{{ $dish->cover_image }}" class="card-img-top" alt="...">
@@ -28,37 +26,42 @@
                         class="card-link btn btn-primary fw-bold">Modifica piatto</a>
 
                     <button class="btn btn-danger fw-bold" type="button" data-bs-toggle="modal"
-                        data-bs-target="#exampleModal">
+                        data-bs-target="#dishModal{{ $dish->id }}">
                         Elimina
                     </button>
                 </div>
             </div>
-        @endforeach
 
-        <div class="modal fade text-primary" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
-            aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h1 class="modal-title fs-4" id="exampleModalLabel">Elimina piatto</h1>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        Sicuro di voler eliminare il piatto?
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary delete-btn"
-                            data-bs-dismiss="modal">CHIUDI<span></span></button>
-                        <form action="{{ route('admin.dishes.destroy', $dish) }}" method="POST">
-                            @csrf
-                            @method('DELETE')
-
-                            <button type="submit" class="btn btn-secondary">ELIMINA<span></span></button>
-                        </form>
+            @if(isset($dish))
+            <div class="modal fade text-primary" id="dishModal{{$dish->id}}" tabindex="-1" aria-labelledby="exampleModalLabel"
+                aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-4" id="exampleModalLabel">Elimina piatto</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            Sicuro di voler eliminare il piatto?
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary delete-btn"
+                                data-bs-dismiss="modal">CHIUDI<span></span></button>
+                            <form action="{{ route('admin.dishes.destroy', $dish) }}" method="POST">
+                                @csrf
+                                @method('DELETE')
+    
+                                <button type="submit" class="btn btn-secondary">ELIMINA<span></span></button>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+            @endif
+            
+        @endforeach
+
+
 
     </div>
     <div class="container">
