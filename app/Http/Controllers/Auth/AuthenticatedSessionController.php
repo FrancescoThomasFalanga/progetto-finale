3cda051c19b467c628f1452f4caf31d13f770727
FrancescoThomasFalanga <francescoasdf3124@gmail.com>
2023-06-15 12:40:30 +0200

    fix: auth correction

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use App\Models\Restaurant;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -23,11 +24,17 @@ class AuthenticatedSessionController extends Controller
     /**
      * Handle an incoming authentication request.
      */
-    public function store(LoginRequest $request): RedirectResponse
+    public function store(LoginRequest $request, Restaurant $restaurant): RedirectResponse
     {
         $request->authenticate();
 
         $request->session()->regenerate();
+
+        if(!Restaurant::where('user_id', Auth::id())->first()) {
+
+            return redirect()->route('admin.restaurants.create');
+
+        }
 
         return redirect()->intended(RouteServiceProvider::HOME);
     }
