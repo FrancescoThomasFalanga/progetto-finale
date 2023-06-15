d5927eb5a847cc993643fa94d32e203d79a717fa
FrancescoThomasFalanga <francescoasdf3124@gmail.com>
2023-06-15 17:54:51 +0200

    feat: add validation

--- resources/views/Admin/restaurants/create.blade.php
+++ resources/views/Admin/restaurants/create.blade.php
@@ -3,13 +3,13 @@
 @section('content')
     <h1 class="m-4 text-center">Aggiungi un ristorante</h1>
 
-    <form action="{{ route('admin.restaurants.store') }}" method="POST" enctype="multipart/form-data">
+    <form class="container" action="{{ route('admin.restaurants.store') }}" method="POST" enctype="multipart/form-data">
         @csrf
 
-        <div class="mb-3">
+        <div class="mb-4">
             <label for="activity_name">Nome dell'attività</label>
             <input type="text" name="activity_name" id="activity_name"
-                class="form-control @error('activity_name') is-invalid @enderror" value="{{ old('activity_name') }}" required>
+                class="form-control @error('activity_name') is-invalid @enderror" value="{{ old('activity_name') }}" ddddd>
             @error('activity_name')
                 <div class="invalid-feedback">
                     {{ $message }}
@@ -17,10 +17,10 @@
             @enderror
         </div>
 
-        <div class="mb-3">
+        <div class="mb-4">
             <label for="phone_number">Numero di telefono</label>
             <input type="tel" name="phone_number" {{-- pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" --}} id="phone_number"
-                class="form-control @error('phone_number') is-invalid @enderror" value="{{ old('phone_number') }}" required>
+                class="form-control @error('phone_number') is-invalid @enderror" value="{{ old('phone_number') }}" ddddd>
             @error('phone_number')
                 <div class="invalid-feedback">
                     {{ $message }}
@@ -28,10 +28,10 @@
             @enderror
         </div>
 
-        <div class="mb-3">
+        <div class="mb-4">
             <label for="address">Indirizzo</label>
             <input type="text" name="address" id="address" class="form-control @error('address') is-invalid @enderror"
-                value="{{ old('address') }}" required>
+                value="{{ old('address') }}" ddddd>
             @error('address')
                 <div class="invalid-feedback">
                     {{ $message }}
@@ -39,10 +39,10 @@
             @enderror
         </div>
 
-        <div class="mb-3">
+        <div class="mb-4">
             <label for="vat">Partita IVA</label>
             <input type="number" name="vat" id="vat" class="form-control @error('vat') is-invalid @enderror"
-                value="{{ old('vat') }}" required>
+                value="{{ old('vat') }}" ddddd>
             @error('vat')
                 <div class="invalid-feedback">
                     {{ $message }}
@@ -50,28 +50,27 @@
             @enderror
         </div>
 
-        <div class="mb-3 form-group">
+        <div class="mb-4 form-group">
             <p>Tipologia del locale</p>
             @foreach ($types as $type)
-                <div class="form-check">
-                    <input type="checkbox" name="types[]" id="type-{{ $type->id }}"
-                        value="{{ $type->id }}" @checked(in_array($type->id, old('types', [])))>
+                <div class="form-check @error('types') is-invalid @enderror">
+                    <input type="checkbox" class="form-check-input" name="types[]" id="type_{{ $type->id }}" value="{{ $type->id }}" @checked(in_array($type->id, old('types', [])))>
                     <label for="type-{{ $type->id }}">{{ $type->name }}</label>
                 </div>
             @endforeach
-            @error('creation_date')
+            @error('types')
                 <div class="invalid-feedback">
                     {{ $message }}
                 </div>
             @enderror
         </div>
 
-        <div class="mb-3">
+        <div class="mb-4">
             <label for="cover_image" class="">Copertina ristorante</label>
             <input name="cover_image" id="cover_image" type="file"
                 class="form-control @error('cover_image') is-invalid @enderror">
             @error('cover_image')
-                <div class="invalid-feedback mb-3 mt-0"> {{ $message }} </div>
+                <div class="invalid-feedback mb-4 mt-0"> {{ $message }} </div>
             @enderror
         </div>
 
