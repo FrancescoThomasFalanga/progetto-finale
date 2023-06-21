6b85ab8a8009513227b3a66cd68bc07ed8f22727
FrancescoThomasFalanga <francescoasdf3124@gmail.com>
2023-06-21 11:10:14 +0200

    feat: add html to notFound view

--- resources/views/Admin/notFound.blade.php
+++ resources/views/Admin/notFound.blade.php
@@ -1,1 +1,53 @@
-PAGINA NON TROVATA
+@extends('layouts/app')
+
+@section('content')
+ 
+<section class="wrapper">
+
+    <div class="container">
+
+        <div id="scene" class="scene" data-hover-only="false">
+
+
+            <div class="circle" data-depth="1.2"></div>
+
+            <div class="one" data-depth="0.9">
+                <div class="content">
+                    <span class="piece"></span>
+                    <span class="piece"></span>
+                    <span class="piece"></span>
+                </div>
+            </div>
+
+            <div class="two" data-depth="0.60">
+                <div class="content">
+                    <span class="piece"></span>
+                    <span class="piece"></span>
+                    <span class="piece"></span>
+                </div>
+            </div>
+
+            <div class="three" data-depth="0.40">
+                <div class="content">
+                    <span class="piece"></span>
+                    <span class="piece"></span>
+                    <span class="piece"></span>
+                </div>
+            </div>
+
+            <p class="p404" data-depth="0.50">404</p>
+            <p class="p404" data-depth="0.10">404</p>
+
+        </div>
+
+        <div class="text">
+            <article>
+                <p>Uh oh! Sembra che tu ti sia perso. <br>Torna alla homepage se hai capito!</p>
+                <a class="" href="{{ url('/') }}">Homepage!</a>
+            </article>
+        </div>
+
+    </div>
+</section>
+
+@endsection
