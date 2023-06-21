fd7bcdcc1569fa561688f42ce40eb6c4ad82963f
FrancescoThomasFalanga <francescoasdf3124@gmail.com>
2023-06-21 11:10:28 +0200

    fix: fixed homepage route

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -38,7 +38,7 @@
                             </a>
 
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ url('admin') }}">{{__('Homepage')}}</a>
+                                <a class="dropdown-item" href="{{ url('/') }}">{{__('Homepage')}}</a>
                                 <a class="dropdown-item" href="{{ url('profile') }}">{{__('Profile')}}</a>
                                 <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
