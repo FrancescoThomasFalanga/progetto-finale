cd0ee7b112258ff75cb7677a69a77b2c210ab1e5
Aranel9966 <99angelo.dire@gmail.com>
2023-06-23 15:40:46 +0200

    feat: add table leads

---
+++ resources/views/Admin/emails/new-contact.blade.php
@@ -0,0 +1,7 @@
+<h1>Ciao...</h1>
+<p>
+    Sei stato contattato da {{ $lead->name }} <br>
+    Nome: {{ $lead->name }} <br>
+    Email: {{ $lead->email }} <br>
+    Messaggio: {{ $lead->messaggio }} <br>
+</p>
