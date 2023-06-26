84e167d8597016414f1dd8b09fe660c2e6bdbfe1
FrancescoThomasFalanga <francescoasdf3124@gmail.com>
2023-06-26 10:25:17 +0200

    feat: add if restaurants null

--- app/Http/Controllers/Admin/StatController.php
+++ app/Http/Controllers/Admin/StatController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Order;
 use App\Models\Restaurant;
+use App\Models\Type;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -13,19 +14,32 @@ class StatController extends Controller
     public function index() {
 
         $user_id = Auth::id();
-        $restaurant = Restaurant::where('user_id', $user_id)->first();
-        $restaurantID = $restaurant->id;
+        $restaurants = Restaurant::where('user_id', $user_id)->first();
+
+        if ($restaurants == null) {
+
+            $types = Type::all();
+
+            return view('admin.restaurants.create', compact('types'));
+
+        } else {
+
+            $restaurantID = $restaurants->id;
+            
+            $orders = Order::with('dishes')
+            ->select('orders.*')
+            ->join('dish_order', 'orders.id', '=', 'dish_order.order_id')
+            ->join('dishes', 'dish_order.dish_id', '=', 'dishes.id')
+            ->where('dishes.restaurant_id', $restaurantID )
+            ->distinct()
+            ->get();
         
-        $orders = Order::with('dishes')
-        ->select('orders.*')
-        ->join('dish_order', 'orders.id', '=', 'dish_order.order_id')
-        ->join('dishes', 'dish_order.dish_id', '=', 'dishes.id')
-        ->where('dishes.restaurant_id', $restaurantID )
-        ->distinct()
-        ->get();
-    
-    
-        return view('admin/stats/index', compact('orders'));
+        
+            return view('admin/stats/index', compact('orders'));
+
+        }
+
+
 
     }
 }
