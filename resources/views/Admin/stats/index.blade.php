644731d3b6c09f999d8c882466286aa8ba69b5a3
FrancescoThomasFalanga <francescoasdf3124@gmail.com>
2023-06-26 13:06:51 +0200

    feat: add dynamic stats

--- resources/views/Admin/stats/index.blade.php
+++ resources/views/Admin/stats/index.blade.php
@@ -1,79 +1,92 @@
 @extends('layouts/app')
 
 @section('content')
-    
+
 <canvas id="myChart" class="bg-white m-5 container"></canvas>
 
-<script>
-    // setup 
-    const data = {
-      labels: ['01/23', '02/23', '03/23', '04/23', '05/23', '06/23', '07/23', '08/23', '09/23', '10/23', '11/23', '12/23'],
-      datasets: [{
-        label: 'Vendite mensili',
-        data: [
-                18,
-                12, 
-                6, 
-                9, 
-                12, 
-                3, 
-                50,
-                2,
-                22,
-                23,
-                11,
-                60,
-            ],
-        backgroundColor: [
-          'rgba(255, 26, 104, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
-          'rgba(0, 0, 0, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
-          'rgba(0, 0, 0, 0.2)',
-        ],
-        borderColor: [
-          'rgba(255, 26, 104, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)',
-          'rgba(0, 0, 0, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)',
-          'rgba(0, 0, 0, 1)'
-        ],
-        borderWidth: 1
-      }]
+
+<h1 class="text-center"> Guadagno totale: 
+
+  @php
+
+    $totalArray = [];
+    
+    foreach ($total as $singleTotal) {
+      
+      $totalArray[] = $singleTotal['total'];
+      
     };
 
-    // config 
-    const config = {
+    $all = array_sum($totalArray);
+
+    echo $all . '€';
+
+  @endphp
+
+</h1>
+
+<script>
+  const orderCounts = @json($orderCounts);
+  const months = @json($months);
+  
+  const labels = months.map(month => {
+      const monthString = month.toString().padStart(2, '0');
+      return `${monthString}/23`;
+  });
+
+  const data = {
+      labels: labels,
+      datasets: [{
+          label: 'Vendite mensili',
+          data: orderCounts,
+          backgroundColor: [
+              'rgba(255, 26, 104, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+              'rgba(0, 0, 0, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+              'rgba(0, 0, 0, 0.2)',
+          ],
+          borderColor: [
+              'rgba(255, 26, 104, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+              'rgba(0, 0, 0, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+              'rgba(0, 0, 0, 1)'
+          ],
+          borderWidth: 1
+      }]
+  };
+
+  const config = {
       type: 'bar',
       data,
       options: {
-        scales: {
-          y: {
-            beginAtZero: true
+          scales: {
+              y: {
+                  beginAtZero: true
+              }
           }
-        }
       }
-    };
+  };
 
-    // render init block
-    const myChart = new Chart(
+  const myChart = new Chart(
       document.getElementById('myChart'),
       config
-    );
+  );
 </script>
 
 @endsection
