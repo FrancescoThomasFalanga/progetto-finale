0ac62ce91cf5483142505eec50767b7b220dad73
FrancescoThomasFalanga <francescoasdf3124@gmail.com>
2023-06-26 10:25:12 +0200

    feat: add if restaurants null

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Order;
 use App\Models\Restaurant;
+use App\Models\Type;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -18,20 +19,30 @@ class OrderController extends Controller
     public function index()
     {   
         $user_id = Auth::id();
-        $restaurant = Restaurant::where('user_id', $user_id)->first();
-        $restaurantID = $restaurant->id;
-        
-        $orders = Order::with('dishes')
-        ->select('orders.*')
-        ->join('dish_order', 'orders.id', '=', 'dish_order.order_id')
-        ->join('dishes', 'dish_order.dish_id', '=', 'dishes.id')
-        ->where('dishes.restaurant_id', $restaurantID )
-        ->distinct()
-        ->get();
+        $restaurants = Restaurant::where('user_id', $user_id)->first();
 
-       
+        if($restaurants == null) {
 
-        return view('admin/orders/index', compact('orders'));
+            $types = Type::all();
+
+            return view('admin.restaurants.create', compact('types'));
+            
+        } else {
+            
+            $restaurantID = $restaurants->id;
+            
+            $orders = Order::with('dishes')
+            ->select('orders.*')
+            ->join('dish_order', 'orders.id', '=', 'dish_order.order_id')
+            ->join('dishes', 'dish_order.dish_id', '=', 'dishes.id')
+            ->where('dishes.restaurant_id', $restaurantID )
+            ->distinct()
+            ->get();
+    
+            return view('admin/orders/index', compact('orders'));
+
+        }
+
     }
 
     /**
