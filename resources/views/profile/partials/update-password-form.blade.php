031c0be75976bf671ce64857425a2aac165e88ea
FrancescoThomasFalanga <francescoasdf3124@gmail.com>
2023-06-29 12:59:06 +0200

    feat: add style in profile.updatepassword

--- resources/views/profile/partials/update-password-form.blade.php
+++ resources/views/profile/partials/update-password-form.blade.php
@@ -1,10 +1,10 @@
 <section>
-    <header>
-        <h2 class="text-lg font-medium text-gray-900">
+    <header class="d-flex flex-column gap-2 align-items-start">
+        <h2 class="text-lg font-medium text-gray-900 label-bg text-white p-2">
             {{ __('Update Password') }}
         </h2>
 
-        <p class="mt-1 text-sm text-gray-600">
+        <p class="mt-1 text-sm text-gray-600 label-bg text-white p-2">
             {{ __('Ensure your account is using a long, random password to stay secure.') }}
         </p>
     </header>
@@ -14,7 +14,7 @@
         @method('put')
 
         <div class="mb-2">
-            <label for="current_password">{{__('Current Password')}}</label>
+            <label class="text-white fw-bold label-bg py-1 mt-2 mb-1" for="current_password">{{__('Current Password')}}</label>
             <input class="mt-1 form-control" type="password" name="current_password" id="current_password" autocomplete="current-password">
             @error('current_password')
             <span class="invalid-feedback mt-2" role="alert">
@@ -24,7 +24,7 @@
         </div>
 
         <div class="mb-2">
-            <label for="password">{{__('New Password')}}</label>
+            <label class="text-white fw-bold label-bg py-1 mt-2 mb-1" for="password">{{__('New Password')}}</label>
             <input class="mt-1 form-control" type="password" name="password" id="password" autocomplete="new-password">
             @error('password')
             <span class="invalid-feedback mt-2" role="alert">
@@ -35,7 +35,7 @@
 
         <div class="mb-2">
 
-            <label for="password_confirmation">{{__('Confirm Password')}}</label>
+            <label class="text-white fw-bold label-bg py-1 mt-2 mb-1" for="password_confirmation">{{__('Confirm Password')}}</label>
             <input class="mt-2 form-control" type="password" name="password_confirmation" id="password_confirmation" autocomplete="new-password">
             @error('password_confirmation')
             <span class="invalid-feedback mt-2" role="alert">
@@ -45,7 +45,7 @@
         </div>
 
         <div class="d-flex align-items-center gap-4">
-            <button type="submit" class="btn btn-primary">{{ __('Save') }}</button>
+            <button type="submit" class="btn btn-dark">{{ __('Save') }}</button>
 
             @if (session('status') === 'password-updated')
             <script>
