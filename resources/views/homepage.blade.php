1c1f229fb05e0bc2f5f1c5af625bf1292e2b83a8
FrancescoThomasFalanga <francescoasdf3124@gmail.com>
2023-06-29 16:11:25 +0200

    feat: add style

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-<div class="jumbotron">
+<div class="jumbotron" style="height: 100vh">
     <div class="container py-5">
         <div class="logo_deliveboo">
             <img src="{{ Vite::asset('resources/img/logo-deliveboo.png') }}" alt="">
