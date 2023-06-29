b7ff7902d57b5ef9c9415ef54a096de2fae733a7
FrancescoThomasFalanga <francescoasdf3124@gmail.com>
2023-06-29 12:58:52 +0200

    feat: add style in profile.edit

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -4,20 +4,20 @@
 <div class="container">
     <div class="fs-4 my-4">
     </div>
-    <div class="card p-4 mb-4 bg-white shadow rounded-lg border-5 border-warning rounded-5">
+    <div class="card p-4 mb-4 background shadow rounded-lg border-5 border-warning rounded-5">
 
         @include('profile.partials.update-profile-information-form')
 
     </div>
 
-    <div class="card p-4 mb-4 bg-white shadow rounded-lg border-5 border-warning rounded-5">
+    <div class="card p-4 mb-4 background shadow rounded-lg border-5 border-warning rounded-5">
 
 
         @include('profile.partials.update-password-form')
 
     </div>
 
-    <div class="card p-4 mb-4 bg-white shadow rounded-lg border-5 border-warning rounded-5">
+    <div class="card p-4 mb-4 background shadow rounded-lg border-5 border-warning rounded-5">
 
 
         @include('profile.partials.delete-user-form')
